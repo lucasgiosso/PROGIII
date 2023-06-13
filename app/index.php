0102e8cbbb7bb2c43545c22aa4de4ccdc80d8504
Lucas <lucasdiazgiosso@gmail.com>
2023-06-13 19:18:17 -0300

    Prueba Funcional

--- app/index.php
+++ app/index.php
@@ -38,7 +38,7 @@ $app->group('/usuarios', function (RouteCollectorProxy $group) {
   });
 
 $app->get('[/]', function (Request $request, Response $response) {    
-    $payload = json_encode(array("mensaje" => "Slim Framework 4 PHP"));
+    $payload = json_encode(array("mensaje" => "Funciona"));
     
     $response->getBody()->write($payload);
     return $response->withHeader('Content-Type', 'application/json');
